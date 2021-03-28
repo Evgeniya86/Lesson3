0f941ce25000fd314af6dbe440f8932898384b62
Evgeniya86 <81212751+Evgeniya86@users.noreply.github.com>
2021-03-28 20:56:31 +0400

    Add files via upload

---
+++ Apple.java
@@ -0,0 +1,8 @@
+package lesson3_1;
+
+public class Apple extends Fruit {
+    @Override
+    float getWeight() {
+        return 1.0f;
+    }
+}
---
+++ Fruit.java
@@ -0,0 +1,5 @@
+package lesson3_1;
+
+public abstract class Fruit {
+    abstract float getWeight();
+}
